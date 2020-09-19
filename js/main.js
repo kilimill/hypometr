c4220bd25a605ba97e2772e5837ee2f858034efb
kilimill <kilimill2585@gmail.com>
2020-09-19 16:26:39 +0700

    start

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function() {
+
+  
+  
+
+  $('.burger-menu').on('click', function(){
+    $('.left-bar').slideToggle();
+  });
+
+  $('.drop-down').on('click', function () {
+    $('.button-info').slideToggle();
+  });
+  
+
+  $('#button-info__select').styler({});
+  
+  
+
+});
+
+
+
